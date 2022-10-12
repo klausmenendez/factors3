a3708613c323943124653dc0c0d1d287c63d2b92
klausmenendez <114318894+klausmenendez@users.noreply.github.com>
2022-10-12 11:45:17 -0700

    Update factors.py

--- factors.py
+++ factors.py
@@ -5,7 +5,6 @@
 
 print("Please enter a positive integer:")
 num1=int(input())
-print("The factors of", num1, "are:")
 for i in range(1,num1+1):
     if num1%i==0 and (num1/i)%2==0:
         print(i)
